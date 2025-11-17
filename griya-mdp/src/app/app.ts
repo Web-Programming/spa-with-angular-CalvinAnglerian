82848a4c762768dab19bcaf487d158219a326c41
CalvinAnglerian <calvinangelriansusanto24@gmail.com>
2025-11-17 12:54:44 +0700

    houseComponent, DataList dan DinamicData Binding

--- griya-mdp/src/app/app.ts
+++ griya-mdp/src/app/app.ts
@@ -1,11 +1,24 @@
+// import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.html',
+//   styleUrl: './app.css'
+// })
+// export class App {
+//   protected readonly title = signal('griya-mdp');
+// }
+
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('griya-mdp');
