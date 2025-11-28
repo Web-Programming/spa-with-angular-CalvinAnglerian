deefcb7d949041753597351c08e6dad6fa564458
CalvinAnglerian <calvinangelriansusanto24@gmail.com>
2025-11-28 11:16:37 +0700

    contact_ts_html

--- griya-mdp/src/app/contact/contact.ts
+++ griya-mdp/src/app/contact/contact.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
+  contactForm: FormGroup;
+
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      nama: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      pesan: ['', [Validators.required, Validators.minLength(10)]], 
+    })
+  }
+
+  submitContact(): void {
+    if (this.contactForm.valid) {
+      const formData = this.contactForm.value;
+      console.log('Form submitted', formData);
+
+      // TODO: Kirim data ke backend API
+      // this.authService.register(formData).subscribe(...)
+    } else {
+      console.log('Form is not valid');
+    }
+  }
   //Tugas : Implementasikan form kontak di Contact Component
   //Gunakan Reactive Forms dengan validasi untuk:
   //Nama (required, min 2 karakter)
